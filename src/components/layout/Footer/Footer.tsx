db7c8b916456d30d19a784f7b2987b461a2d228a
Burulsun <burulsunadylbekova8@gmail.com>
2024-11-08 21:56:20 +0600

    footer-details

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,23 +1,79 @@
-import { FC } from "react";
+"use client";
+import { FC, useState } from "react";
 import scss from "./Footer.module.scss";
 import { FaInstagram } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
-{/* <FaTwitter /> */}
-{/* <FaInstagram /> */}
-{/* <FaFacebook /> */}
-
+{
+    /*  */
+}
+{
+    /* <FaInstagram /> */
+}
+{
+    /*  */
+}
 
 const Footer: FC = () => {
- return (
-  <footer className={scss.Footer}>
-   <div className="container">
-    <div className={scss.content}>
-        <h1>footer</h1>
-    </div>
-   </div>
-  </footer>
- );
+    const [showAll, setShowAll] = useState(false);
+    return (
+        <footer className={scss.Footer}>
+            <div className="container">
+                <div className={scss.content}>
+                    <div
+                        className={scss.company}
+                        style={{
+                            width: "100px",
+                        }}
+                    >
+                        <h3>Компания</h3>
+                        <b className={showAll ? scss.notCart : scss.cart}>
+                            О нас
+                        </b>
+                        <b>Вакансии</b>
+                        <b>For the Record</b>
+                    </div>
+                    <div
+                        className={scss.company}
+                        style={{
+                            width: "160px",
+                        }}
+                    >
+                        <h3>Сообщества</h3>
+                        <b>Для исполнителей</b>
+                        <b>Для разработчиков</b>
+                        <b>Реклама</b>
+                        <b>Для инвесторов</b>
+                        <b>Для вендоров</b>
+                    </div>
+                    <div className={scss.company}>
+                        <h3>Полезные ссылки</h3>
+                        <b>Справка</b>
+                        <b>Бесплатное мобильное приложение</b>
+                    </div>
+                    <div className={scss.company}>
+                        <h3>Планы Spotify</h3>
+                        <b>Индивидуальная подписка Spotify Peremium</b>
+                        <b>Premium для двоих</b>
+                        <b>Premium для семьи</b>
+                        <b>Premium для студентов</b>
+                        <b>Бесплатная версия Spotify</b>
+                    </div>
+                    <div className={scss.companyySite}>
+                        <a href="https://www.instagram.com/spotify">
+                            <FaInstagram />
+                        </a>
+                        <a href="https://x.com/spotify">
+                            <FaTwitter />
+                        </a>
+                        <a href="https://www.facebook.com/Spotify">
+                            <FaFacebook />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/components/pages/HomeSections/Home.tsx
+++ src/components/pages/HomeSections/Home.tsx
@@ -1,16 +1,47 @@
-import { FC } from "react";
+"use client";
+import { FC, useState } from "react";
 import scss from "./Home.module.scss";
+import { RiSpotifyFill } from "react-icons/ri";
+import { MdOutlinePlayCircleFilled } from "react-icons/md";
+import { MdOutlinePauseCircleFilled } from "react-icons/md";
+
+
 
 const Home: FC = () => {
- return (
-  <section className={scss.Home}>
-   <div className="container">
-    <div className={scss.content}>
-        <h1>home</h1>
-    </div>
-   </div>
-  </section>
- );
+    const [fill, setFill] = useState(false);
+    const [play, setPlay] = useState(false);
+    return (
+        <section className={scss.Home}>
+            <div className="container">
+                <div className={scss.content}>
+                    <div className={fill ? scss.homes : scss.home}>
+                        <a>
+                            <RiSpotifyFill />
+                        </a>
+                        <img
+                            src="https://i.guim.co.uk/img/media/67944850a1b5ebd6a0fba9e3528d448ebe360c60/359_0_2469_1482/master/2469.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=1157be337c6e200937b038797d772f5f"
+                            alt=""
+                        />
+                        <a className={scss.homes} onClick={() => setPlay(!play)}>
+                            {
+                                play? (
+                                    <MdOutlinePauseCircleFilled />
+                                ) : (
+                                    <MdOutlinePlayCircleFilled />
+                                )
+                            }
+                        </a>
+                        <b>Billie Eilish</b>
+                        <h3>
+                            The most essential R&B songs from the '00s. Cover:
+                            Usher
+                        </h3>
+                    </div>
+                  
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Home;
